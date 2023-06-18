a25ac4983f911350fa03daa79e3f592936daf592
jchalare <albertchalare@gmail.com>
2023-06-18 14:15:02 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, } from '@nestjs/common';
+
 
 async function nestInventory() {
   const app = await NestFactory.create(AppModule);
-
+  
   app.setGlobalPrefix('api');
+
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
